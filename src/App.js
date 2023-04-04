441eb983e88ca519320d70c4165dd0a8d5ac11a5
Kanhaiya Kumar <kanhaiyak42@gmail.com>
2023-04-04 20:25:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import StopWatch from "./components/Stopwatch";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <StopWatch />
+    </div>
+  );
+}
---
+++ src/components/Stopwatch.js
@@ -0,0 +1,62 @@
+import { useEffect, useRef, useState } from "react";
+
+const StopWatch = () => {
+  const [timer, setTimer] = useState(0);
+  const [isStarted, setStart] = useState(false);
+  const timerRef = useRef();
+  useEffect(() => {
+    return () => {
+      clearInterval(timerRef.current);
+    };
+  }, []);
+
+  const startTimer = () => {
+    timerRef.current = setInterval(() => {
+      setTimer((prevTimer) => setTimer((prevTimer += 1)));
+    }, 1000);
+    setStart(true);
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerRef.current);
+    setStart(false);
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+    setTimer(0);
+  };
+
+  return (
+    <div className="stopwatch">
+      <div>
+        {Math.floor(timer / 3600)} hh :: {Math.floor(timer / 60)} mm ::{" "}
+        {Math.floor(timer % 60)} ss
+      </div>
+      <div className="stopwatch-control">
+        <button
+          onClick={(_) => startTimer()}
+          disabled={isStarted}
+          className="primary"
+        >
+          Start
+        </button>
+        <button
+          onClick={(_) => stopTimer()}
+          disabled={!isStarted}
+          className="primary"
+        >
+          Stop
+        </button>
+        {
+          <button onClick={(_) => resetTimer()} className="primary">
+            Reset
+          </button>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default StopWatch;
